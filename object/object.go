6674535e6b89edb090c7bdd5386bba169f4b92ff
Yuri <yurisamp123@gmail.com>
2024-07-07 17:31:16 -0300

    finish the basic interpreter

--- object/object.go
+++ object/object.go
@@ -23,25 +23,6 @@ const (
 	FUNCTION_OBJ = "FUNCTION"
 )
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store : s}
-}
-
-type Environment struct {
-	store map[string]Object
-}
-
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-	return val
-}
-
-func(e *Environment) Get(name string) (Object, bool){
-	obj, ok := e.store[name]
-	return obj, ok
-}
-
 type Integer struct {
 	Value int64
 }
