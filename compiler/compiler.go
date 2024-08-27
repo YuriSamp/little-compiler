cc9cc98baaa71e7e5ae52cd572b6ab2df09ed9fb
Yuri <yurisamp123@gmail.com>
2024-08-27 01:35:43 -0300

    feat: add suport for arrays

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -157,6 +157,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		default: 
 			return fmt.Errorf("unknown Operator %s", node.Operator)
 		}
+	case *ast.ArrayLiteral:
+		for _, el := range node.Elements {
+			err := c.Compile(el)
+			if err != nil {
+				return err
+			}
+		}	
+		c.emit(code.OpArray, len(node.Elements))
 	case *ast.PrefixExpression:
 		err := c.Compile(node.Right)
 		
