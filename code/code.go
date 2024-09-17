31a09c61c2c2054a3a78da8a67166bce3f20d736
Yuri <yurisamp123@gmail.com>
2024-09-16 21:55:34 -0300

    feat: add local scope to symbol table

--- code/code.go
+++ code/code.go
@@ -34,6 +34,8 @@ const (
 	OpNull
 	OpGetGlobal
 	OpSetGlobal
+	OpSetLocal
+	OpGetLocal
 	OpArray
 	OpHash
 	OpIndex
@@ -61,6 +63,8 @@ var definitions = map[Opcode]*Definition{
 	OpNull : {"OpNull", []int{}},
 	OpGetGlobal : {"OpGetGlobal", []int{2}},
 	OpSetGlobal : {"OpSetGlobal", []int{2}},
+	OpGetLocal : {"OpGetLocal", []int{1}},
+	OpSetLocal : {"OpSetLocal", []int{1}},
 	OpArray: {"OpArray", []int{2}},
 	OpHash : {"OpHash", []int{2}},
 	OpIndex : {"OpIndex", []int{}},
@@ -103,6 +107,8 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instuctions[offset:], uint16(o))
+		case 1:
+			 instuctions[offset] = byte(o)	
 		}
 		offset += width
 	}
@@ -118,6 +124,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int){
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))	
 		}
 		
 		offset += width
@@ -126,8 +134,12 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int){
 	return operands, offset
 }
 
-func ReadUint16 (ins Instructions) uint16 {
+func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func (ins Instructions) String() string {
